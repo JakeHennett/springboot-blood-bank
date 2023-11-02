238d913da3c44b1cfbb4562795fe1f5f1dc56e88
Jake Hennett <jake.hennett@gmail.com>
2023-11-01 20:44:31 -0400

    response logic returned

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -1,9 +1,6 @@
 async function submit() {
-    //alert("test");
     var donorgroup = document.getElementById("donor-group").value;
-    //alert(donorgroup);
     var donorrh = document.getElementById("donor-rh-factor").value;
-    //alert(donorrh);
     var patientgroup = document.getElementById("patient-group").value;
     var patientrh = document.getElementById("patient-rh-factor").value;
     var log = 
@@ -13,23 +10,26 @@ async function submit() {
     + " \npatient group " + patientgroup
     + " \npatinet rh factor " + patientrh
     + ".";
+    console.log(log);
     //alert(log);
     var group = await groupCheck(donorgroup, patientgroup);
     var rh = await rhCheck(donorrh, patientrh);
-    alert("group " + group +
-    "\nrh " + rh);
-    // response = "results: " + group
-    // alert(response);
+    //alert("group " + group + "\nrh " + rh);
+    var decision = await donationCheck(group, rh);
+    alert(decision);
 }
 
 async function groupCheck(donor, patient) {
     console.log("group: donor: " + donor + "\npatient: " + patient);
-    //alert("group function");
-    //alert(donor + " " + patient);
     var group = false;
+
     if (donor == "o")
         group = true;
     else if (patient == "ab")
+        group = true;
+    else if (donor == "a" && patient == "a")
+        group = true;
+    else if (donor == "b" && patient == "b")
         group = true;
 
     return group;
@@ -39,5 +39,23 @@ async function rhCheck(donor, patient) {
     console.log("rh: donor: " + donor + "\npatient: " + patient);
     var rh = false;
 
+    if (donor == "negative")
+        rh = true;
+    else if (patient == "positive")
+        rh = true;
+
     return rh;
+}
+
+async function donationCheck(group, rh) {
+    decision = "";
+
+    if (group && rh)
+        decision = "Safe donation.";
+    if (!group)
+        decision = "Blood group conflict.";
+    if (!rh)
+        decision = "Rh factor conflict.";
+
+    return decision;
 }
